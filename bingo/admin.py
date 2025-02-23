a34e32239a9cf02c2ca98ccbd407c3279fa8c7e0
Myo Sandar <90026694@study.curtincollege.edu.au>
2025-02-23 13:29:37 +0000

    admin pannel for bingo.admin.py

--- bingo/admin.py
+++ bingo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import Challenge, UserChallenge,  Reward
 
 # Register your models here.
 from django.urls import path, include
@@ -7,4 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),  # Django Admin Panel
     path('api/', include('bingo.urls')),  # Includes all API routes from the 'bingo' app
 ]
- 
+
+admin.site.register(Challenge)
+admin.site.register(UserChallenge)
+admin.site.register(Reward)
