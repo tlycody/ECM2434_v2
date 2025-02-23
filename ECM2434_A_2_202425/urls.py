5ac5dc8368d44e16583d963556ce08f0ef61f9ad
Laurenconz <laurennchao@gmail.com>
2025-02-23 12:48:00 +0000

    updated urls and views

--- ECM2434_A_2_202425/urls.py
+++ ECM2434_A_2_202425/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
+
+# Test homepage
+def home(request):
+    return HttpResponse("Hello, Django is running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django Admin Panel
-    path('', include('bingo.urls')),  # Includes all API routes from the 'bingo' app
+    path('', home),  # Homepage at '/'
+    path('bingo/', include('bingo.urls')),  # Includes all API routes from the 'bingo' app
 ]
 
+# Debug Toolbar (Only if settings.DEBUG is True)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
--- bingo/urls.py
+++ bingo/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path
-from bingo.views import login_user, get_user_profile, complete_task,leaderboard
+from django.http import HttpResponse
+from .views import login_user, get_user_profile, complete_task, leaderboard, RegisterUserView, TasksView
 from rest_framework_simplejwt.views import TokenRefreshView
-from django.conf.urls import include
-from django.conf import settings
-from .views import RegisterUserView
-from .views import TasksView
+
+# New home view for the bingo app
+def bingo_home(request):
+    return HttpResponse("Welcome to the Bingo API!")
 
 urlpatterns = [
+    path('', bingo_home, name='bingo_home'),  # ✅ This ensures /bingo/ works
     path('register_user/', RegisterUserView.as_view(), name='register'),
     path('login/', login_user, name='login'),
     path('user/', get_user_profile, name='user'),
-    path('api/token/refresh/', TokenRefreshView.as_view(),name ='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/tasks/', TasksView.as_view(), name='tasks'),
     path('complete_task/', complete_task, name='complete_task'),
     path('leaderboard/', leaderboard, name='leaderboard'),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
