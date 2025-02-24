14e41ed360d5d91ba7a09f9900d903e698a07977
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-24 17:22:13 +0000

    unit test

--- bingo/tests.py
+++ bingo/tests.py
@@ -160,7 +160,7 @@ class ViewsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("error", response.data)
 
-    def test_register_user_invalid_email(self):
+    def test_register_passwords_mismatch(self):
         data = {
             "username": "newuser",
             "password": "pass1",
