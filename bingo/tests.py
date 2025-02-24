4372de0fe83e41402c932978c217820088c5290b
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-24 17:22:07 +0000

    unit test

--- bingo/tests.py
+++ bingo/tests.py
@@ -163,9 +163,9 @@ class ViewsTestCase(APITestCase):
     def test_register_user_invalid_email(self):
         data = {
             "username": "newuser",
-            "password": "StrongPassw0rd!",
-            "passwordagain": "StrongPassw0rd!",
-            "email": "newuser@gmail.com"
+            "password": "pass1",
+            "passwordagain": "pass2",
+            "email": "newuser@exeter.ac.uk"
         }
         response = self.client.post('/api/register/', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
