200e8efe92e7be640092c4833d81f6b9d269bd5d
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-24 23:09:47 +0000

    .

--- bingo/views.py
+++ bingo/views.py
@@ -12,6 +12,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+
 from .models import Task, UserTask, Leaderboard, Profile
 from .serializers import TaskSerializer, LeaderboardSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
