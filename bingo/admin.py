812897be2cd57c23545e0c2fc2897b469df36138
sandra <88787255+myosandarkyaw18@users.noreply.github.com>
2025-02-24 16:45:12 +0000

    Update admin.py

--- bingo/admin.py
+++ bingo/admin.py
@@ -2,14 +2,11 @@ from .models import Task, UserTask, Leaderboard
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.register(Task)
-admin.site.register(UserTask)
-admin.site.register(Leaderboard)
-
-
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django Admin Panel
     path('api/', include('bingo.urls')),  # Includes all API routes from the 'bingo' app
 ]
+
+admin.site.register(Task)
+admin.site.register(UserTask)
+admin.site.register(Leaderboard)
