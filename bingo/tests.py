095df7b6ace021a08a59ee61226f421c4572b2a1
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-24 17:22:17 +0000

    unit test

--- bingo/tests.py
+++ bingo/tests.py
@@ -169,7 +169,7 @@ class ViewsTestCase(APITestCase):
         }
         response = self.client.post('/api/register/', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("detail", response.data)
+        self.assertIn("Passwords do not match", response.data.get("error", ""))
 
     def test_register_user_passwords_mismatch(self):
         data = {
