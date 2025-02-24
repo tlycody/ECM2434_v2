bb028e555468a71baaf67fa3db6d8ed692eac4e9
Siemon-Cha <sc1332@exeter.ac.uk>
2025-02-24 23:10:46 +0000

    test

--- bingo/tests.py
+++ bingo/tests.py
@@ -6,18 +6,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase, APIRequestFactory
 from django.core.files.uploadedfile import SimpleUploadedFile
-from .models import Task, UserTask, Leaderboard, Profile
-from .views import (
-    email_validation,
-    register_user,
-    login_user,
-    tasks,
-    complete_task,
-    leaderboard,
-    get_user_profile,
-    update_user_profile,
-    user_rank
-)
+from .models import Profile, Task, UserTask, Leaderboard
+from .views import email_validation, get_client_ip
 
 User = get_user_model()
 
