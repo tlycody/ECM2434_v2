d156f564caad0d8e48d71a9ca125b8b3be68ee5e
Siemon-Cha <sc1332@exeter.ac.uk>
2025-03-08 03:55:50 +0000

    setup for strong password built-in

--- ECM2434_A_2_202425/settings.py
+++ ECM2434_A_2_202425/settings.py
@@ -86,10 +86,28 @@ DATABASES = {
 
 # 8️⃣ Password Validation
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {
+            'user_attributes': ('username', 'email', 'first_name', 'last_name'),
+            'max_similarity': 0.7,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+    {
+        'NAME': 'bingo.validators.CustomPasswordValidator',
+    },
 ]
 
 AUTH_USER_MODEL = 'bingo.User' 
