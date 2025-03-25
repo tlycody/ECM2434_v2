f0d5863ae47480a0dfe99706c6e2dd199d05dc6a
Laurenconz <laurennchao@gmail.com>
2025-03-25 07:03:39 +0000

    ALLOWED_HOSTS = ['*']

--- ECM2434_A_2_202425/settings.py
+++ ECM2434_A_2_202425/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-fallback-secret-key')
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 TIME_ZONE = 'Europe/London'
 
 # 3️⃣ Installed Apps
